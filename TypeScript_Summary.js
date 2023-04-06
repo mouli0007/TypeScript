258def30166a274e285e78ee033eb02b80128ec6
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-04-06 16:01:35 +0530

    Create TypeScript_Summary.js

---
+++ TypeScript_Summary.js
@@ -0,0 +1,29 @@
+
+######## TypeScript ##########
+
+####TypeScript is a Super Set of JS
+
+### All TypeScript File Complied down to regular JS
+
+### Advantages
+
+ More Robust
+ Easily Spot  Bugs
+ Predictability
+ Readability
+ Popular
+ 
+ ### Disadvantage 
+ 
+ More Code to Write
+ Browser wont support TS file so we have to complie it down
+ 
+ 
+ ### Compiling TypeScript 
+ 
+ TypeScript uses .ts and .tsx extensions
+ TS is used to compile .ts file to js 
+ can watch and report errors at complie time
+ tsconfig.json file is used to configure how typescript works
+ 
+ 
