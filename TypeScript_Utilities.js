f2a7b6603c6ccad3a27c8048b80cfe49bc7b37f6
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-04-08 00:28:59 +0530

    Update TypeScript_Utilities.js

--- TypeScript_Utilities.js
+++ TypeScript_Utilities.js
@@ -22,16 +22,93 @@ const assign_1: Assignment = {
   grade: 90,
 };
 
-const assign_2: Assignment = {
-  studentId: "Comp",
-  title: "Final Project",
-  grade: 0,
-};
-
 console.log(updateAssignment(assign_1, { grade: 78 }));
 
-const another = (obj: Assignment, props: Partial<Assignment>): Assignment => {
-  return { ...obj, ...props };
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+//  Required
+
+const recordAssignment = (assign: Required<Assignment>): Assignment => {
+  // Send to database
+
+  return { ...assign };
 };
 
-console.log(another(assign_2, { grade: 12, title: "Mid-Year Project !" }));
+console.log(
+  recordAssignment({
+    studentId: "co27416",
+    title: "Big final Project",
+    grade: 90,
+    verified: false,
+  })
+);
+
+// ReadOnly !
+//  We cant modify the data
+const assignVerified: Readonly<Assignment> = {
+  ...assign_1,
+  verified: true,
+};
+
+console.log(assignVerified.grade);
+
+//  Record Types !!!
+
+const hexColor: Record<string, string> = {
+  red: "red",
+  green: "green",
+  blue: "blue",
+};
+
+type students = "Sara" | "Kelly";
+
+type LetterGrades = "A" | "B" | "C" | "D" | "U";
+
+const finalGrades: Record<students, LetterGrades> = {
+  Sara: "B",
+  Kelly: "C",
+};
+
+console.log(finalGrades);
+
+interface Grades {
+  assign1: number;
+  assign2: number;
+}
+
+const getData: Record<students, Grades> = {
+  Sara: { assign1: 89, assign2: 23 },
+  Kelly: { assign1: 1, assign2: 12 },
+};
+
+console.log(getData);
+
+// Pick and Omit !
+// Getting what we want from the object !
+type AssignResult = Pick<Assignment, "studentId" | "grade">;
+
+const score: AssignResult = {
+  studentId: "co27416",
+  grade: 98,
+};
+
+Object.values(score).map((key) => console.log(key));
+
+// Omit will do the opposite of Pick !
+
+type AssignPrev = Omit<Assignment, "grade" | "verified">;
+
+const omm: AssignPrev = {
+  studentId: "co00000",
+  title: "Hatrford",
+};
+
+console.log(omm);
+
+//  And Much more Utilities were there
+
+// Return
+// Parameters
+// Exclude and Extract !
