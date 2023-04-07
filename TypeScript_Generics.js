3f501a93962bacd28d24d7e3678e615338d31b4f
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-04-07 22:09:46 +0530

    Update TypeScript_Generics.js

--- TypeScript_Generics.js
+++ TypeScript_Generics.js
@@ -21,7 +21,10 @@ console.log(numa_);
 Extending the Interface with Generics !
   
   
-  interface HasId {
+
+//  Generic Extension !
+
+interface HasId {
   id: number;
 }
 
@@ -30,3 +33,34 @@ const processUser = <T extends HasId>(user: T): T => {
 };
 
 console.log(processUser({ id: 99, name: "Mouli VJ" }));
+
+const ownProcessor = <T extends HasId>(na: T): T => {
+  console.log(na);
+  return na;
+};
+
+ownProcessor({ id: 89, name: "Cobra", job: "Assasian" });
+
+// Generic Extension with classes !
+
+class StateObj<T> {
+  private data: T;
+
+  constructor(value: T) {
+    this.data = value;
+  }
+
+  get state(): T {
+    return this.data;
+  }
+
+  set state(value: T) {
+    this.data = value;
+  }
+}
+
+const store = new StateObj("Mouli VJ");
+
+console.log(store.state);
+store.state = "Alison";
+console.log(store.state);
